4e1780ae1d42d3b6c7c78cf4179dfa8fbcfcaf1b
Rali Dimitrov <ralidimitrovdimitrov@gmail.com>
2018-09-30 17:39:12 +0300

    commit 05

--- application/views/admin/page/order.php
+++ application/views/admin/page/order.php
@@ -4,17 +4,27 @@
 	<div id="orderResult">
 		
 	</div>
+	<input type="button" id='save' value="Save" class="btn btn-primary" />
 </section>
 
 <script>
 	$(function(){
 		$.post(
-
 			"<?php echo site_url('admin/page/order_ajax'); ?>",
 			{},
 			function(data) {
 				$('#orderResult').html(data);
 			}
 		);
+		$('#save').click(function(){
+			oSortable = $('.sortable').nestedSortable('toArray');
+			// $.post(
+			// 	"<?php echo site_url('admin/page/order_ajax'); ?>",
+			// 	{ soratble: oSortable},
+			// 	function(data) {
+			// 		$('#orderResult').html(data);
+			// 	}
+			// )
+		});
 	});
 </script>
